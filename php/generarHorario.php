c8342895565db3b83560217861a64639e7b66501
iyessan445 <143215657+isayeste@users.noreply.github.com>
2024-05-03 22:30:49 +0200

    guardarHorariosEnBaseDatos

--- php/generarHorario.php
+++ php/generarHorario.php
@@ -1,5 +1,46 @@
 <?php
 
+    function guardarInicioFin($entrada, $salida, $fechaActual, $duracionCita, $duracion){
+        $inicio = strtotime(date('Y-m-d', $fechaActual) . ' ' . $entrada);
+        $fin = strtotime(date('Y-m-d', $fechaActual) . ' ' . $salida);
+        // echo date("Y-m-d H:i:s", $inicio). "<br>";
+        // echo date("Y-m-d H:i:s", $fin). "<br>";
+        // echo "<hr>";
+        while($inicio<=$fin){
+            $inicioCita = $inicio;
+            $finCita = strtotime("+" . $duracionCita . " minutes", $inicioCita);
+            $fechaInicio = date("Y-m-d H:i:s", $inicioCita);
+            $fechaFin = date("Y-m-d H:i:s", $finCita);
+            // echo $duracionCita. "<br>";
+            // echo date("Y-m-d H:i:s", $inicioCita). "<br>";
+            // echo date("Y-m-d H:i:s", $finCita). "<br><hr>";
+            $inicio =  strtotime("+" . $duracion . " minutes", $inicio);
+            // --------------------------------------------------------------------------------
+            //Conexión con la base de datos
+            $servidor = 'localhost';
+            $usuario = 'root';
+            $password = "";
+            $nombreBD = 'psyconnect';
+
+            try{
+                $conexion = new PDO("mysql:host=$servidor;dbname=$nombreBD", $usuario, $password);
+                $sql = "INSERT INTO horarios (fechaInicio, fechaFin, estado) VALUES (?, ?, ?)";
+                //Preparar la consulta y devuelve un  objeto PDOStatement. execute ejecuta la consulta
+                $stmt = $conexion->prepare($sql);
+                $stmt->execute([$fechaInicio, $fechaFin, "libre"]);
+            
+            
+            }
+            catch(PDOException $e){
+                echo "Error al insertar los datos ". $e ->getMessage();
+            }
+            finally {
+            // Cerrar la conexión
+                $conexion = null;
+            }
+        }
+    }
+
     //Datos del formulario
     $dias = $_POST["dias"];
     $entradaManana = $_POST["entradaManana"];
@@ -24,23 +65,7 @@
     $fechaFinTrimestre = strtotime("+3 months", $fechaActual);
 
 
-    function guardarInicioFin($entrada, $salida, $fechaActual, $duracionCita, $duracion){
-        $inicio = strtotime(date('Y-m-d', $fechaActual) . ' ' . $entrada);
-        $fin = strtotime(date('Y-m-d', $fechaActual) . ' ' . $salida);
-        // echo date("Y-m-d H:i:s", $inicio). "<br>";
-        // echo date("Y-m-d H:i:s", $fin). "<br>";
-        // echo "<hr>";
-        while($inicio<=$fin){
-            $inicioCita = $inicio;
-            $finCita = strtotime("+" . $duracionCita . " minutes", $inicioCita);
-            $fechaInicio = date("Y-m-d H:i:s", $inicioCita);
-            $fechaFin = date("Y-m-d H:i:s", $finCita);
-            echo $duracionCita. "<br>";
-            echo date("Y-m-d H:i:s", $inicioCita). "<br>";
-            echo date("Y-m-d H:i:s", $finCita). "<br><hr>";
-            $inicio =  strtotime("+" . $duracion . " minutes", $inicio);
-        }
-    }
+    
 
     //Iterar sobre cada día entre la fecha actual y la fecha final del trimestre
     while($fechaActual<=$fechaFinTrimestre){
